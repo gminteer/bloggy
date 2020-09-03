83b4305963c9052568e0b24c51b468e301f2b1dd
gminteer <gminteer@gmail.com>
2020-09-03 14:51:51 -0400

    Revert "first pass: user routes"
    This reverts commit 08266f1e0d4a187a80dbe729a40756ea8b6a061a.

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-module.exports = (models, sequelize) => {
-  router.use('/users', require('./user-routes')(models, sequelize));
-  router.use('/posts', require('./post-routes')(models, sequelize));
-  return router;
-};
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-module.exports = (models, sequelize) => {
-  router.use('/api', require('./api')(models, sequelize));
-  router.use('/', require('./home-routes')(models, sequelize));
-  router.use((_, res) => res.sendStatus(404));
-  return router;
-};
